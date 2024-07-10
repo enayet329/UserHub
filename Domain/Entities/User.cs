9a3160e966248bd592d9763cd181b204511a0357
enayet329 <mdsakayethossen2@gmail.com>
2024-07-11 04:02:51 +0600

    Add project files.

---
+++ Domain/Entities/User.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Domain.Entities
+{
+    internal class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; }
+        public bool IsBlocked { get; set; } = false; 
+        public bool IsDeleted { get; set; } = false;
+    }
+}
