9e733c3c0640f29c7212643af0c5bd904261cfbf
enayet329 <mdsakayethossen2@gmail.com>
2024-07-11 04:26:30 +0600

    I have created user Entitie

--- Domain/Entities/User.cs
+++ Domain/Entities/User.cs
@@ -1,5 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Domain.Entities
 {
@@ -7,13 +7,18 @@ namespace Domain.Entities
     {
         [Key]
         public int Id { get; set; }
+
         public string Name { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
+
         public string Password { get; set; } = string.Empty;
-        public string City { get; set; } = string.Empty;
+
         public DateTime CreatedAt { get; set; } = DateTime.Now;
-        public DateTime UpdatedAt { get; set; }
-        public bool IsBlocked { get; set; } = false; 
+
+        public DateTime LastLogin { get; set; } 
+
+        public bool IsBlocked { get; set; } = false;
+
         public bool IsDeleted { get; set; } = false;
     }
 }
