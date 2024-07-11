074d1a3086d483ca814377e96a380ed4ad72d56c
enayet329 <mdsakayethossen2@gmail.com>
2024-07-11 12:16:11 +0600

    Add UserHubContext class for managing user data

---
+++ Infrastructure/Data/UserHubContext.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class UserHubContext : DbContext
+    {
+        public UserHubContext(DbContextOptions<UserHubContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<User> UserHub { get; set; }
+    }
+}
