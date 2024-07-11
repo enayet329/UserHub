2e0b75d26d6db03e7d6a44b5143bc6103fee8a5d
enayet329 <mdsakayethossen2@gmail.com>
2024-07-11 12:19:50 +0600

    Add dependency injection setup for UserHubContext
    - Introduced ServicesContainer class with AddServices extension method.
    - Configured IServiceCollection to add UserHubContext as a scoped service.
    - Used Microsoft.EntityFrameworkCore to set up SQL Server database connection.
    - MigrationsAssembly specified for Entity Framework Core migrations.

---
+++ Infrastructure/DependencyInjection/ServicesContainer.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Infrastructure.DependencyInjection
+{
+    public static class ServicesContainer
+    {
+        public static void AddServices(this IServiceCollection services,IConfiguration configuration)
+        {
+            services.AddDbContext<UserHubContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("UserHubConnection"),
+                b => b.MigrationsAssembly(typeof(ServicesContainer).Assembly.FullName)),
+                ServiceLifetime.Scoped);
+
+
+        }
+    }
+}
