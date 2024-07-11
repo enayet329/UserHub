6a24a3ea17b0fef536dbcef36c6f421a52fa685c
enayet329 <mdsakayethossen2@gmail.com>
2024-07-11 12:21:54 +0600

    Configure dependency injection and services setup in program startup
    - Added singleton configuration for IConfiguration.
    - Introduced service configuration using AddServices extension method.
    - Utilized IConfiguration to provide configuration settings across services.

--- UserHub.API/Program.cs
+++ UserHub.API/Program.cs
@@ -1,6 +1,11 @@
+using Infrastructure.DependencyInjection;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddSingleton<IConfiguration>(builder.Configuration);
+builder.Services.AddServices(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
